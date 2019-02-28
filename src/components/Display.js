08ccc9476db11fbb173c7283cb71b788fc031feb
lauraalpinto <lauripri@gmail.com>
2019-02-28 15:30:22 +0000

    Init + Form and Display Components
    Creating the Form + Display + basic SCSS.

---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+  render(){
+
+    return(
+      <div className="Display">Display</div>
+    )
+  }
+}
+export default Display;
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render(){
+    return(
+      <div className="Form">Form</div>
+    )
+  }
+}
+export default Form;
