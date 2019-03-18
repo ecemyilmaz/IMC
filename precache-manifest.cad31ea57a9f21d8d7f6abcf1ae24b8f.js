131c297fcf129b04f07d644eeb5e337b20746229
lauraalpinto <lauripri@gmail.com>
2019-03-18 12:14:32 +0000

    Updates

---
+++ precache-manifest.cad31ea57a9f21d8d7f6abcf1ae24b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ae7d13fab2e1be7931",
+    "url": "/IMC/static/js/runtime~main.19ae7d13.js"
+  },
+  {
+    "revision": "4ac44a3f92d1b6378bc7",
+    "url": "/IMC/static/js/main.4ac44a3f.chunk.js"
+  },
+  {
+    "revision": "d90c75215989876323b7",
+    "url": "/IMC/static/js/2.d90c7521.chunk.js"
+  },
+  {
+    "revision": "4ac44a3f92d1b6378bc7",
+    "url": "/IMC/static/css/main.6d7223ca.chunk.css"
+  },
+  {
+    "revision": "4bd70b12814206c3c1d7dc0aea985bb4",
+    "url": "/IMC/index.html"
+  }
+];
